c3a5066105e570bbeb88ac8a78b9844569ce7742
David Blankenship <dblanken@iupui.edu>
2021-05-11 22:13:50 -0400

    Remove bat

--- shell.rc/aliases.bash
+++ shell.rc/aliases.bash
@@ -7,7 +7,7 @@ alias '???'='bing'
 # overrides
 # Have to keep these here due to ~/.bin already being in the path
 # Did not want to remove and re-add to beginning
-alias cat='bat'
+# alias cat='bat'
 alias irb='irb --readline -r irb/completion'
 alias ls='ls -GFh'
 alias mkdir='mkdir -p'
