91bb748ad4385d05c52c7a46de7ca8f3c038243b
David Blankenship <dblanken@iupui.edu>
2021-05-12 09:40:37 -0400

    Make asdf smarter

--- shell.rc/asdf.sh
+++ shell.rc/asdf.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-source /usr/local/opt/asdf/asdf.sh
+if [ -f /usr/local/opt/asdf/asdf.sh ]; then
+  source /usr/local/opt/asdf/asdf.sh
+elif [ -f $HOME/.asdf/asdf.sh ]; then
+  source $HOME/.asdf/asdf.sh
+else
+  echo "I did not detect an asdf instance."
+fi
