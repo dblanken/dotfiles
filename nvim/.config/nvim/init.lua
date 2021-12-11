e0ffb67da2099dc312b6f1fffd48161d5e578128
David Blankenship <dblanken@iu.edu>
2021-12-11 11:22:41 -0500

    add(nvim): Add zsh filetype to */.zsh/*

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -113,6 +113,7 @@ augroup dblanken_filetypes
   autocmd FileType ruby setlocal comments=:#\ tw=78
   autocmd BufRead,BufNewFile *.md set filetype=markdown
   autocmd FileType liquid,markdown,text,txt setlocal tw=78 linebreak keywordprg=dict spell
+  autocmd BufRead,BufNewFile */.zsh/* set ft=zsh
 augroup END
 ]]
 
@@ -128,3 +129,75 @@ local function setupGrepProgram()
 end
 
 vim.opt.grepprg = setupGrepProgram()
+
+-- Plugins
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+require('packer').startup(function(use)
+  use "wbthomason/packer.nvim"
+
+  -- His Popeness
+  use "tpope/vim-commentary"
+  use "tpope/vim-surround"
+  use "tpope/vim-rails"
+  use "tpope/vim-projectionist"
+  use "tpope/vim-fugitive"
+  use "tpope/vim-dispatch"
+  use "tpope/vim-unimpaired"
+  use "tpope/vim-bundler"
+  use "tpope/vim-eunuch"
+  use "tpope/vim-rake"
+  use "tpope/vim-vinegar"
+  use "tpope/vim-endwise"
+  use "tpope/vim-repeat"
+  use "tpope/vim-git"
+  use "tpope/vim-markdown"
+  use "tpope/vim-flagship"
+  use "tpope/vim-apathy"
+  use "tpope/vim-dadbod"
+  use "tpope/vim-scriptease"
+  use "tpope/vim-abolish"
+  use "tpope/vim-rhubarb"
+  use "tpope/vim-characterize"
+  use "tpope/vim-dotenv"
+  use "tpope/vim-obsession"
+  use "tpope/vim-speeddating"
+  use "tpope/vim-ragtag"
+  use "tpope/vim-jdaddy"
+  use "tpope/vim-tbone"
+  use "tpope/vim-afterimage"
+
+  -- Themes
+  use "gruvbox-community/gruvbox"
+
+  -- Non-Tpope
+  use "christoomey/vim-tmux-navigator"
+  use "vim-ruby/vim-ruby"
+  use "vim-test/vim-test"
+  use "dense-analysis/ale"
+  use "tribela/vim-transparent"
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
+vim.cmd [[
+nmap <silent> <leader>t :TestNearest<CR>
+nmap <silent> <leader>T :TestFile<CR>
+nmap <silent> <leader>a :TestSuite<CR>
+nmap <silent> <leader>l :TestLast<CR>
+let test#strategy = 'dispatch'
+
+nmap <silent> ]d <Plug>(ale_next_wrap)
+nmap <silent> [d <Plug>(ale_previous_wrap)
+let g:ale_echo_msg_format = '[%linter%] %code: %%s'
+
+colorscheme gruvbox
+]]
