6ebe9ceba7aafce40391edeb415874c822c876e4
David Blankenship <dblanken@iu.edu>
2022-03-01 14:54:55 -0500

    feat(nvim): Add global printing/reloading
    Taken from tjdevries, this will help in debugging and viewing tables in
    Lua: https://github.com/tjdevries/config_manager/blob/master/xdg_config/nvim/lua/tj/globals.lua

--- nvim/.config/nvim/lua/dblanken/globals.lua
+++ nvim/.config/nvim/lua/dblanken/globals.lua
@@ -12,3 +12,17 @@ vim.g.clipboard = {
 }
 
 vim.g.ruby_fold = 0
+
+P = function(v)
+  print(vim.inspect(v))
+  return v
+end
+
+RELOAD = function(...)
+  return require("plenary.reload").reload_module(...)
+end
+
+R = function(name)
+  RELOAD(name)
+  return require(name)
+end
