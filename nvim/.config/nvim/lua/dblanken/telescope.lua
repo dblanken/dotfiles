36a79ec640a8f5e13022b52326f4f3e893cc6b20
David Blankenship <dblanken@iu.edu>
2022-03-01 13:18:01 -0500

    feat(nvim): Add neovim/dotfile navigation

--- nvim/.config/nvim/lua/dblanken/telescope.lua
+++ nvim/.config/nvim/lua/dblanken/telescope.lua
@@ -4,3 +4,6 @@ vim.api.nvim_set_keymap('n', '<Leader>ff', '<cmd>lua require("telescope.builtin"
 vim.api.nvim_set_keymap('n', '<Leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<Leader>fb', '<cmd>lua require("telescope.builtin").buffers()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<Leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<CR>', opts)
+
+vim.api.nvim_set_keymap('n', '<Leader>en', '<cmd>lua require("dblanken.telescope_customizations").edit_neovim()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>ed', '<cmd>lua require("dblanken.telescope_customizations").edit_dotfiles()<CR>', opts)
