ac7be19f1b60f7d94e5ceec73d21d6f4e6884859
David Blankenship <dblanken@iu.edu>
2022-03-02 07:39:04 -0500

    fix(nvim): Add <CR> to vim-test bindings

--- nvim/.config/nvim/lua/dblanken/test.lua
+++ nvim/.config/nvim/lua/dblanken/test.lua
@@ -1,8 +1,8 @@
 local opts = { silent = true }
 
-vim.api.nvim_set_keymap('n', '<leader>t', ':TestNearest', opts)
-vim.api.nvim_set_keymap('n', '<leader>T', ':TestFile', opts)
-vim.api.nvim_set_keymap('n', '<leader>a', ':TestSuite', opts)
-vim.api.nvim_set_keymap('n', '<leader>l', ':TestLast', opts)
+vim.api.nvim_set_keymap('n', '<leader>t', ':TestNearest<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>T', ':TestFile<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>a', ':TestSuite<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>l', ':TestLast<CR>', opts)
 
 vim.g["test#strategy"] = 'dispatch'
