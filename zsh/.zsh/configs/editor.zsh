c24f91eac5491488382e96fe991b6c6021ac12bf
David Blankenship <dblanken@iu.edu>
2022-10-26 07:39:41 -0400

    Add VIMCONFIG and VIMDATA environment variables

--- zsh/.zsh/configs/editor.zsh
+++ zsh/.zsh/configs/editor.zsh
@@ -1,8 +1,14 @@
 # Editor
 #
 
+export VIMCONFIG="$HOME/.local/share/vim"
+export VIMDATA="$VIMCONFIG/data"
+
 export EDITOR="vim"
 export VISUAL="$EDITOR"
 
 alias v='$EDITOR'
 alias vimrc='$EDITOR ~/.vimrc'
+
+mkdir -p "$VIMCONFIG"
+mkdir -p "$VIMDATA"
