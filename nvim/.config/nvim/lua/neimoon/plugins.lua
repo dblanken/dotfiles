5116b0d40828642e28ad93d60c4dcfcd00a6c51f
David Blankenship <david.blankenship@yale.edu>
2023-07-20 14:32:37 -0400

    Remove neoai

--- nvim/.config/nvim/lua/neimoon/plugins.lua
+++ nvim/.config/nvim/lua/neimoon/plugins.lua
@@ -172,30 +172,6 @@ require("lazy").setup({
   { 'fgheng/winbar.nvim' },
   { 'SmiteshP/nvim-navic' },
   {
-    "Bryley/neoai.nvim",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-    },
-    cmd = {
-      "NeoAI",
-      "NeoAIOpen",
-      "NeoAIClose",
-      "NeoAIToggle",
-      "NeoAIContext",
-      "NeoAIContextOpen",
-      "NeoAIContextClose",
-      "NeoAIInject",
-      "NeoAIInjectCode",
-      "NeoAIInjectContext",
-      "NeoAIInjectContextCode",
-    },
-    keys = {
-      { "<leader>as", desc = "summarize text" },
-      { "<leader>ag", desc = "generate git message" },
-    },
-    config = true,
-  },
-  {
     'Wansmer/treesj',
     keys = {
       {
