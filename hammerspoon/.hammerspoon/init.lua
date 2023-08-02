7f11c6522dfa3b02e62eb42b9f41d276fa79ee37
David Blankenship <david.blankenship@yale.edu>
2023-08-02 10:11:45 -0400

    fix(hammerspoon): Replace amazon music with fluid
    Fluid using the web page takes less resources than the app, so I'm using
    that for now.

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -119,7 +119,7 @@ local numberOpenings = {
 	'com.apple.mail',
 	'com.apple.MobileSMS',
 	'com.microsoft.teams',
-	'com.amazon.music',
+	'com.fluidapp.FluidApp2.Amazon Music Web',
 }
 
 local layoutConfig = {
