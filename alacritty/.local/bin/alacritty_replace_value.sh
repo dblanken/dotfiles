ca4bfaca1c9cc66e38b3e6239d05504a1fc434fe
David Blankenship <david.blankenship@yale.edu>
2023-08-02 09:02:49 -0400

    add(alacritty): Add scripts to alacritty

---
+++ alacritty/.local/bin/alacritty_replace_value.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Specify the path to the alacritty.yml file
+config_file="$HOME/.config/alacritty/alacritty.yml"
+replace_value="${2:-1.0}"
+search_value="${1:-0.85}"
+
+# Backup the original file
+cp "$config_file" "$config_file.bak"
+
+# Use awk to replace the opacity value
+awk -v search="opacity: $search_value" -v replace="opacity: $replace_value" '$0 ~ search {gsub(search, replace)}1' "$config_file.bak" > "$config_file"
+
+echo "Opacity value replaced successfully."
+
