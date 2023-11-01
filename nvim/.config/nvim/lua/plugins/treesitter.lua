9eebedd2e9af9e441e74d7337315d00f81d7bd20
David Blankenship <david.blankenship@yale.edu>
2023-11-01 15:16:21 -0400

    add(treesitter): add javascript to pre-install

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {                                                   -- better highlightin
     config = function()
       require('nvim-treesitter.configs').setup {
         -- Add languages to be installed here that you want installed for treesitter
-        ensure_installed = { 'lua', 'python', 'tsx', 'typescript', 'vimdoc', 'vim', 'ruby', 'php', 'twig', 'css', 'html' },
+        ensure_installed = { 'lua', 'python', 'tsx', 'typescript', 'vimdoc', 'vim', 'ruby', 'php', 'twig', 'css', 'html', 'javascript' },
 
         -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
         auto_install = false,
