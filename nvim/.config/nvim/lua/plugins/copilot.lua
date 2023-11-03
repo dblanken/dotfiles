2691f1e0257a82e3f14208bbf96894ab90194752
David Blankenship <david.blankenship@yale.edu>
2023-11-03 08:51:46 -0400

    add(copilot): add debug placeholder

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -4,6 +4,7 @@ return {
   'zbirenbaum/copilot.lua',
   commit = '1a8032ae496916ccc7a7a52ee79194fbef29f462',
   config = function()
+    -- vim.lsp.set_log_level('debug')
     require('copilot').setup({
       suggestion = { enabled = false },
       panel = { enabled = false },
